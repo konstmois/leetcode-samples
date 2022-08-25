b356b2ffa68cfb66cbb8b46ea45b0d17bf6dff31
konstmois <moiskonst@yandex.ru>
2022-08-25 22:26:46 +0200

    3. Longest Substring Without Repeating Characters

--- src/main/kotlin/LongestSubstringWithoutRepeat.kt
+++ src/main/kotlin/LongestSubstringWithoutRepeat.kt
@@ -1,33 +1,29 @@
 import java.util.*
+import kotlin.collections.HashSet
+import kotlin.math.max
+
 // 3. Longest Substring Without Repeating Characters
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/
 class LongestSubstringWithoutRepeat {
 
     fun lengthOfLongestSubstring1(s: String): Int {
-        var result = LinkedList<Char>()
-        var size = 0
-        for (i in s.indices) {
-            val currentChar: Char = s[i]
-            val temp = LinkedList<Char>()
-            var finish = false
-            if (result.isNotEmpty()) {
-                do {
-                    val currentInWindow = result[0]
-                    temp.add(result.removeFirst())
-                    if (currentInWindow == currentChar) {
-                        finish = true
-                    } else if (result.isEmpty()) {
-                        finish = true
-                        result = temp
-                    }
-                } while (!finish)
+        var windowStart = 0
+        var windowEnd = 0
+        var result = 0
+        val contentsOfWindow = HashSet<Char>()
+        while (windowEnd < s.length) {
+            if (contentsOfWindow.contains(s[windowEnd])) {
+                contentsOfWindow.remove(s[windowStart])
+                windowStart++
+            } else {
+                contentsOfWindow.add(s[windowEnd])
+                windowEnd++
+                result = Math.max(contentsOfWindow.size, result)
             }
-            result.add(currentChar)
-            if (size < result.size) {
-                size = result.size
-            }
+
         }
-        return size
+        return result
+
     }
 
     fun lengthOfLongestSubstring2(s: String): Int {
